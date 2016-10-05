0bcfb45554eae820ce3fae3233484aa2b7acb4af
Petz <jpetz@amazon.com>
2016-10-05 23:07:20 +1100

    Configuring license files

---
+++ test.rs
@@ -0,0 +1,12 @@
+// Copyright 2015-2016 Jarrod Petz.
+// See the file at the top-level directory of this distribution
+// <COPYRIGHT http://rust-lang.org/COPYRIGHT>
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> 
+// OR the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>
+// at your option.
+//
+// This file may not be copied, modified, or distributed except according to
+// those terms.
